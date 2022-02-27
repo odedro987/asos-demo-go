b1da6b23b44032252e33e0c6005453bd508c9697
Eylon <eylon.perets@gmail.com>
2022-02-27 23:03:27 +0200

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log('Serving on port 3000!');
+});
+
+module.exports = app;
