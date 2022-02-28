c7bb6d6c10bc9b9b9a8c0e2dde45029f1119db18
Eylon <eylon.perets@gmail.com>
2022-02-28 11:55:39 +0200

    navbar categories

---
+++ public/javascripts/navbarLogic.js
@@ -0,0 +1,10 @@
+const categoryMen = document.querySelector('#category-men');
+const categoryWomen = document.querySelector('#category-women');
+
+function selectedCategory() {
+  categoryMen.classList.toggle('category--active');
+  categoryWomen.classList.toggle('category--active');
+}
+
+categoryMen.addEventListener('click', selectedCategory);
+categoryWomen.addEventListener('click', selectedCategory);
