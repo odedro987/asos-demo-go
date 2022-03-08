a12d55a628276f443d692cc9583edd3f9c5ed90e
Eylon <eylon.perets@gmail.com>
2022-03-08 22:50:44 +0200

    products page + filter (not done)

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const ejsMate = require('ejs-mate');
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +15,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/home', (req, res) => {
   res.render('landing');
+});
+
+app.get('/products', (req, res) => {
+  res.render('products');
 });
 
 app.get('*', (req, res) => {
